58c7ddefc3f6d9a248e066b45e1c8a634c9f1cfa
Mostafa Mohab El Rafey <mostafa_mohab18@hotmail.com>
2024-09-28 21:20:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let acc = document.getElementsByClassName("accordion");
     for (let i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            let panel = this.parentElement.parentElement.children[this.parentElement.parentElement.children.length - 1];
+            let panel = this.parentElement.children[this.parentElement.children.length - 1];
             if (panel.style.display === "block") {
                 panel.style.display = "none";
             } else {
