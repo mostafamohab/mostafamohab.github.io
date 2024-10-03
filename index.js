c50d6aae825ab761d02b2de7c539e2716a3b851a
Mostafa Mohab El Rafey <mostafa_mohab18@hotmail.com>
2024-10-03 12:44:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let acc = document.getElementsByClassName("accordion");
-    for (let i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-            let panel = this.parentElement.children[this.parentElement.children.length - 1];
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-                acc[i].children[2].innerHTML = "+";
-            } else {
-                panel.style.display = "block";
-	            acc[i].children[2].innerHTML = "–";
-            }
-        });
-    }
-});
+document.addEventListener("DOMContentLoaded", function () {
+  let acc = document.getElementsByClassName("accordion")
+  for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      let panel =
+        this.parentElement.children[this.parentElement.children.length - 1]
+      if (panel.style.display === "block") {
+        panel.style.display = "none"
+        acc[i].children[2].innerHTML = "+"
+      } else {
+        panel.style.display = "block"
+        acc[i].children[2].innerHTML = "–"
+      }
+    })
+  }
+})
 function generatePDF() {
-        const pdfPath = 'path/to/generated.pdf';
-        const downloadLink = document.getElementById('download-link');
-        downloadLink.href = pdfPath;
+  const pdfPath = "path/to/generated.pdf"
+  const downloadLink = document.getElementById("download-link")
+  downloadLink.href = pdfPath
 }
+
